d4ff1493eec7dbef637942d0a86bdfacf2b70a85
Abel <168181290+Abel-et@users.noreply.github.com>
2026-07-04 19:28:11 +0300

    Add solution for Minimum Score of a Path Between Two Cities

---
+++ leetcode/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,26 @@
+
+from collections import deque, defaultdict
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(list)
+
+        for u, v, w in roads:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+
+        q = deque([1])
+        visited = {1}
+        ans = float('inf')
+
+        while q:
+            node = q.popleft()
+
+            for nei, w in graph[node]:
+                ans = min(ans, w)
+
+                if nei not in visited:
+                    visited.add(nei)
+                    q.append(nei)
+
+        return ans
