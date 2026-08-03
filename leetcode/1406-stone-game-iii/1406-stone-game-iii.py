344e241ce1d4e2d5a2f3db922b987b9396bdb86e
Abel <168181290+Abel-et@users.noreply.github.com>
2026-08-03 14:54:37 +0300

    Add solution for Stone Game III

---
+++ leetcode/1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,30 @@
+
+from functools import lru_cache
+
+class Solution:
+    def stoneGameIII(self, stoneValue):
+        n = len(stoneValue)
+
+        @lru_cache(None)
+        def dp(i):
+            if i >= n:
+                return 0
+
+            best = float("-inf")
+            total = 0
+
+            for k in range(3):
+                if i + k < n:
+                    total += stoneValue[i + k]
+                    best = max(best, total - dp(i + k + 1))
+
+            return best
+
+        ans = dp(0)
+
+        if ans > 0:
+            return "Alice"
+        elif ans < 0:
+            return "Bob"
+        else:
+            return "Tie"
